3a561f1f5582504fa450288d7e283e8b03ba5667
leonardochen01 <leonardochen.lc@gmail.com>
2023-05-02 16:09:45 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
+import Header 
+
 <script setup>
+
 </script>
 
 <template>
 
 <h1>ciao</h1>
 
+<p></p>
+
 </template>
+
 
 <style lang="scss" scoped>
 
 @use './scss/header.scss';
+
+body {
+  
+}
+
+h1 {
+
+}
 
 </style>
